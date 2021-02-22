fcc8a847aae71a281eee4eaee757506641441d41
Mohamed Salama Shakir <salamam301@yahoo.com>
2021-02-22 16:51:35 +0200

    adding some columns to chat rooms and chat messages tables and migrating them to DB
    adding some columns to chat rooms and chat messages tables and migrating them to DB

--- database/migrations/2021_02_22_143011_create_chat_messages_table.php
+++ database/migrations/2021_02_22_143011_create_chat_messages_table.php
@@ -15,7 +15,11 @@ class CreateChatMessagesTable extends Migration
     {
         Schema::create('chat_messages', function (Blueprint $table) {
             $table->id();
+            $table->integer('chat_room_id');
+            $table->integer('user_id');
+            $table->mediumText('message');
             $table->timestamps();
+
         });
     }
 
